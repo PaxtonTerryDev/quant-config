d5e94c36210e23598af686e17aa768efcb6cce30
Paxton Terry <PaxtonTerryDev@gmail.com>
2025-05-14 13:59:43 -0600

    renames class

--- include/quant_config/config.hpp
+++ include/quant_config/config.hpp
@@ -1,32 +1,35 @@
-#ifndef MY_CONFIG_CONFIG_HPP
-#define MY_CONFIG_CONFIG_HPP
+#ifndef QUANT_CONFIG_CONFIG_HPP
+#define QUANT_CONFIG_CONFIG_HPP
 
 #include <iostream>
 
-namespace my_config {
+namespace quant_config
+{
 
-class Config {
-public:
-    Config(int threads = 1, bool logging = false, int precision = 6)
-        : thread_count_(threads), logging_enabled_(logging), precision_(precision) {}
+    class Config
+    {
+    public:
+        Config(int threads = 1, bool logging = false, int precision = 6)
+            : thread_count_(threads), logging_enabled_(logging), precision_(precision) {}
 
-    int getThreadCount() const { return thread_count_; }
-    bool isLoggingEnabled() const { return logging_enabled_; }
-    int getPrecision() const { return precision_; }
+        int getThreadCount() const { return thread_count_; }
+        bool isLoggingEnabled() const { return logging_enabled_; }
+        int getPrecision() const { return precision_; }
 
-    void print() const {
-        std::cout << "Config:\n"
-                  << "  Threads: " << thread_count_ << "\n"
-                  << "  Logging: " << (logging_enabled_ ? "Enabled" : "Disabled") << "\n"
-                  << "  Precision: " << precision_ << " digits" << std::endl;
-    }
+        void print() const
+        {
+            std::cout << "Config:\n"
+                      << "  Threads: " << thread_count_ << "\n"
+                      << "  Logging: " << (logging_enabled_ ? "Enabled" : "Disabled") << "\n"
+                      << "  Precision: " << precision_ << " digits" << std::endl;
+        }
 
-private:
-    int thread_count_;
-    bool logging_enabled_;
-    int precision_;
-};
+    private:
+        int thread_count_;
+        bool logging_enabled_;
+        int precision_;
+    };
 
-} // namespace my_config
+}
 
-#endif // MY_CONFIG_CONFIG_HPP
+#endif // QUANT_CONFIG_CONFIG_HPP
