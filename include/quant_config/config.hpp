2b374e041f68e6b95b0e9da9b52c1edc23d072c6
Paxton Terry <PaxtonTerryDev@gmail.com>
2025-05-14 13:58:18 -0600

    added basic config setup

---
+++ include/quant_config/config.hpp
@@ -0,0 +1,32 @@
+#ifndef MY_CONFIG_CONFIG_HPP
+#define MY_CONFIG_CONFIG_HPP
+
+#include <iostream>
+
+namespace my_config {
+
+class Config {
+public:
+    Config(int threads = 1, bool logging = false, int precision = 6)
+        : thread_count_(threads), logging_enabled_(logging), precision_(precision) {}
+
+    int getThreadCount() const { return thread_count_; }
+    bool isLoggingEnabled() const { return logging_enabled_; }
+    int getPrecision() const { return precision_; }
+
+    void print() const {
+        std::cout << "Config:\n"
+                  << "  Threads: " << thread_count_ << "\n"
+                  << "  Logging: " << (logging_enabled_ ? "Enabled" : "Disabled") << "\n"
+                  << "  Precision: " << precision_ << " digits" << std::endl;
+    }
+
+private:
+    int thread_count_;
+    bool logging_enabled_;
+    int precision_;
+};
+
+} // namespace my_config
+
+#endif // MY_CONFIG_CONFIG_HPP
